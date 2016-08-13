7c53b7790d8b27742d326037ad2af6376d78a770
petrChe <petrchebanenko28@mail.ru>
2016-08-13 20:54:10 +0600

    Initial commit

--- SixthProject/PasswordExeption.cs
+++ SixthProject/PasswordExeption.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SixthProject
-{
+{   [Serializable]
     class PasswordExeption:Exception
     {
         public PasswordExeption() { }
-        public PasswordExeption(string message) : base(message)
-        {
-        }
+        public PasswordExeption(string message) : base(message) { }
+        public PasswordExeption(string message, Exception inner) : base(message, inner) { }
+        protected PasswordExeption(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
